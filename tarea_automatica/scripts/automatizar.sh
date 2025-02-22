897a34955ff78e3b436b3be76d4d8038c2520a0e
DiegoUwUntu <Cacabezas36@ucatolica.edu.co>
2025-02-22 13:51:55 -0500

    Cristian Cabezas - Añadir contenido con echo

--- tarea_automatica/scripts/automatizar.sh
+++ tarea_automatica/scripts/automatizar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ ! -f "tareas.txt" ]; then
   touch tareas.txt
-  chmod 444 tareas.tx
+  chmod 444 tareas.txt
+  echo "Tarea 1: Control de permisos" >> tareas.txt
 else
   cat tareas.txt
 fi
